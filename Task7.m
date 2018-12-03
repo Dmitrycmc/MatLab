9b97e81460b15f5bfa7343fc93aa0969630dff7d
Dmitry Litov <Dmitrycmc@gmail.com>
2018-12-03 08:33:14 +0300

    Ready tasks 1-7

--- Task7.m
+++ Task7.m
@@ -1,13 +1,16 @@
-function fig = Task7(fX, strX, fY, strY, a, b)
+function fig = Task7(x, strX, y, strY, a, b)
     fig = figure('name', ['Task 7: ', 'x = ', strX, '; ','y = ', strY]);
     
     eps = (b - a) / 1000;
     t = a : eps : b;
     
-    x = fX(t);
-    y = fY(t);
+    comet(x(t), y(t));
     
-    comet(x, y);
-    formatFigure(['x = ', strX, '; ','y = ', strY], '', '');
+    grid on;
+    xlabel('x');
+    ylabel('y');
+    
+    title(['Function (', strX, '; ', strY, '):']);
+    legend(['(', strX, '; ', strY, ')']);
 end
 
