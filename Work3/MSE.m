84116ebb77cc29e31950941040038e1f8d430367
Dmitry Litov <Dmitrycmc@gmail.com>
2018-12-06 08:19:08 +0300

    Improved task3->MSE

--- Work3/MSE.m
+++ Work3/MSE.m
@@ -1,12 +1,12 @@
 function res = MSE(img1, img2)
-   [Y, X] = size(img1);
-   [Y1, X1] = size(img2);
-   if Y ~= Y1 || X ~= X1
+   [Y, X, Z] = size(img1);
+   [Y1, X1, Z1] = size(img2);
+   if Y ~= Y1 || X ~= X1 || Z ~= Z1
        res = "Images' sizes don't match!";
    else
        E = img1 - img2;
-       SE = sum(sum(E.^2));
-       res = SE /(X * Y);
+       SE = sum(sum(sum(E.^2)));
+       res = SE /(X * Y * Z);
    end
 end
 
